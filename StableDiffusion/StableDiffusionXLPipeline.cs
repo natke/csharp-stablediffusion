d8f242f2f25f981c0e8ce9b9dacc9d7b7d143909
Nat Kershaw <nakersha@microsoft.com>
2024-01-03 16:14:32 -0800

    Added Unet inputs to pipeline

--- StableDiffusion/StableDiffusionXLPipeline.cs
+++ StableDiffusion/StableDiffusionXLPipeline.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
 using SixLabors.ImageSharp;
+using MathNet.Numerics.Distributions;
 
 namespace StableDiffusion
 {
@@ -9,28 +10,16 @@ namespace StableDiffusion
         OrtValue placeholder;
 
         InferenceSession TokenizerSession;
-        Dictionary<string, OrtValue> TokenizerInputs;
-        Dictionary<string, OrtValue> TokenizerOutputs;
         
         InferenceSession Tokenizer2Session;
-        Dictionary<string, OrtValue> Tokenizer2Inputs;
-        Dictionary<string, OrtValue> Tokenizer2Outputs;
-
+        
         InferenceSession TextEmbeddingSession;
-        Dictionary<string, OrtValue> TextEmbeddingInputs;
-        Dictionary<string, OrtValue> TextEmbeddingOutputs;
-       
+        
         InferenceSession TextEmbedding2Session;
-        Dictionary<string, OrtValue> TextEmbedding2Inputs;
-        Dictionary<string, OrtValue> TextEmbedding2Outputs;
-
+        
         InferenceSession UNetSession;
-        Dictionary<string, OrtValue> UNetInputs;
-        Dictionary<string, OrtValue> UNetOutputs;
 
         InferenceSession VaeDecoderSession;
-        Dictionary<string, OrtValue> VaeDecoderInputs;
-        Dictionary<string, OrtValue> VaeDecoderOutputs;
 
 
         public StableDiffusionXLPipeline(StableDiffusionConfig config)
@@ -74,7 +63,7 @@ namespace StableDiffusion
 
             // Run the tokenizer session
             using var runOptions = new RunOptions();
-            var outputs = tokenizerSession.Run(runOptions, TokenizerInputs, tokenizerSession.OutputNames);
+            var outputs = tokenizerSession.Run(runOptions, tokenizerInputs, tokenizerSession.OutputNames);
 
             // Set the outputs
             var tokenizerOutputs = new Dictionary<string, OrtValue>();
@@ -82,7 +71,7 @@ namespace StableDiffusion
             {
                 tokenizerOutputs[tokenizerSession.OutputNames[i]] = outputs[i];
             }
-            var inputIds = TokenizerOutputs["input_ids"];
+            var inputIds = tokenizerOutputs["input_ids"];
             var inputIdsShape = inputIds.GetTensorTypeAndShape().Shape;
 
             // Check to see if the text embedding model requires int32 token ids.
@@ -109,10 +98,10 @@ namespace StableDiffusion
 
             // Set the inputs
             var textEmbeddingInputs = new Dictionary<string, OrtValue>();
-            TextEmbeddingInputs["input_ids"] = inputIds;
+            textEmbeddingInputs["input_ids"] = inputIds;
 
             // Run the text embedding session
-            outputs = textEmbeddingSession.Run(runOptions, TextEmbeddingInputs, textEmbeddingSession.OutputNames);
+            outputs = textEmbeddingSession.Run(runOptions, textEmbeddingInputs, textEmbeddingSession.OutputNames);
 
             // Set the outputs
             var textEmbeddingOutputs = new Dictionary<string, OrtValue>();
@@ -154,73 +143,67 @@ namespace StableDiffusion
             // 5. Combine the embedding outputs into inputs to the UNet model
             //    - Concatenate the prompt embeddings from the two models
             //    - Use the text embeddings from the second model
+            //    - Calculate the time embeds
 
             var lastHiddenState = promptEmbeds["last_hidden_state"];
             var lastHiddenStateShape = lastHiddenState.GetTensorTypeAndShape().Shape;
             var lastHiddenState2 = promptEmbeds2["last_hidden_state"];
             var lastHiddenState2Shape = lastHiddenState2.GetTensorTypeAndShape().Shape;
 
-            // Last hidden state shape is {batch, sequence length, 1280}
-            // UNet expects 
+            var lastHiddenStateArray = lastHiddenState.GetTensorDataAsSpan<float>().ToArray();
+            var lastHiddenState2Array = lastHiddenState2.GetTensorDataAsSpan<float>().ToArray();
 
-            var buffer = new float[1 * lastHiddenStateShape[1] * lastHiddenStateShape[2] + lastHiddenState2Shape[2]];
+            var buffer = new float[lastHiddenStateArray.Length + lastHiddenState2Array.Length];
+            lastHiddenStateArray.CopyTo(buffer, 0);
+            lastHiddenState2Array.CopyTo(buffer, lastHiddenStateArray.Length);
 
-            // Copy the hidden state into the new buffer
+            long[] combinedHiddenStateShape = {1, lastHiddenStateShape[1], lastHiddenStateShape[2] + lastHiddenState2Shape[2]};
+
+            var combinedHiddenState = OrtValue.CreateTensorValueFromMemory<float>(buffer, combinedHiddenStateShape);
+
+            // Calculate time embeds
+            float[] timeEmbedsBuffer = {config.Height, config.Width, 0, 0, config.Height, config.Width};
+            long[] timeEmbedsShape = { 1, 6 };
+            var timeEmbeds = OrtValue.CreateTensorValueFromMemory<float>(timeEmbedsBuffer, timeEmbedsShape);
+
+            var unetInputs = new Dictionary<string, object>();
+            unetInputs["hidden_states"] = combinedHiddenState;
+            unetInputs["text_embeds"] = promptEmbeds2["text_embeds"];
+            unetInputs["time_embeds"] = timeEmbeds;
+
+            //
+            // 6. Run the Unet model in a loop
+            //
+            long[] latentShape = { 1, 4, config.Width / 4, config.Height / 8 };
+            double[] randomNormal = new double[config.Width * config.Height/2];
+
+            double mean = 100;
+            double stdDev = 10;
+
+            Normal normalDist = new Normal(mean, stdDev);
+            for (int i = 0; i < randomNormal.Length; i++)
+            {
+                randomNormal[i] = normalDist.Sample();
+            }
+
+            var latents = OrtValue.CreateTensorValueFromMemory<double>(randomNormal, latentShape);
+
+            
 
 
-            long[] unetInputShape = { 1, lastHiddenState2Shape[1], lastHiddenStateShape[2] + lastHiddenState2Shape[2] };
 
-            // Creates an OrtValue from a buffer
-            var unetInput = OrtValue.CreateTensorValueFromMemory<float>(buffer, unetInputShape);
+
+
+
+
+
+
 
             //var poolerOutput = textEmbeddingOutputs["pooler_output"];
             //var hiddenStates11 = textEmbeddingOutputs["hidden_state.11"];
 
 
 
-            //UNetInputs["hidden_states"] = ;
-            //UNetInputs["text_embeds"] = ;
-
-
-            // 
-            // 6. Create the time embeddings
-            // 
-            //    return new Tensor(
-            //'float32',
-            // Float32Array.from([height, width, 0, 0, height, width]),
-            // [1, 6],
-            //)
-
-
-
-
-            // Notes:
-
-            // Create a new buffer of a fixed type and length
-            //var buffer = new int[data.Length];
-
-
-            //var length = 2048;
-            //var buffer = new float[length];
-
-            // Create a span over the buffer
-            // 0-1023
-            // 1024-2047
-            // Copy to the span over the buffer.
-
-            //data1.CopyTo(buffer, dimension?);
-
-            //long[] shape2 = { 2, 1024 };
-
-            // Creates an OrtValue from a buffer
-            //var newInput = OrtValue.CreateTensorValueFromMemory(buffer, shape2);
-
-            // Span<float> array1;
-            // Span<float> array2;
-            // Allocate a third buffer
-            // Either array copy or span copy array1 + array2 into array3
-
-            // Span.CopyTo(another span)
 
 
 
